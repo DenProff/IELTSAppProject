e0adcc68de48686c79f805085f69f8962526ef71
Максим Кононов РИС 24-2 <maksimys20066@gmail.com>
2025-05-03 13:48:08 +0300

    Добавление главного и второго окна

---
+++ FirstTestPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IELTSAppProject
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstTestPage.xaml
+    /// </summary>
+    public partial class FirstTestPage : Page
+    {
+        public FirstTestPage()
+        {
+            InitializeComponent();
+
+            foreach (UIElement elem in firstTest.Children)
+            {
+                if (elem is Button)
+                {
+                    if (elem == testVar)
+                        ((Button)elem).Click += Button_Click;
+                    if (elem == @continue)
+                        ((Button)elem).Click += Button_Click_1;
+                    if (elem == turnBack)
+                        ((Button)elem).Click += Button_Click_2;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IELTSAppProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            foreach (UIElement elem in mainScreen.Children)
+            {
+                if (elem is Button)
+                {
+                    if (elem == lessTime)
+                        ((Button)elem).Click += Button_Click;
+                    if (elem == teacherMode)
+                        ((Button)elem).Click += Button_Click_1;
+                    if (elem == developerMode)
+                        ((Button)elem).Click += Button_Click_2;
+                    if (elem == continueWork)
+                        ((Button)elem).Click += Button_Click_3;
+                    if (elem == chooseLanguage)
+                        ((Button)elem).Click += Button_Click_4;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
