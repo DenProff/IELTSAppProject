0b6946e6b20a0fa74ba728dee5cde6e167b978fe
DenProff <farik29261@gmail.com>
2025-05-04 00:29:51 +0500

    Убрана навигационная панель

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,8 +23,7 @@ namespace IELTSAppProject
         public MainWindow()
         {
             InitializeComponent();
-
-            
+            MainFrame.Navigate(new MainPage());
         }
     }
 }
