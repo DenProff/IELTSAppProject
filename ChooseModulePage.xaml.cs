bc183a0abe125720c6fbec97e85d42e9a09e0ba7
Максим Кононов РИС 24-2 <maksimys20066@gmail.com>
2025-05-04 15:00:35 +0300

    Добавление кнопок выбора и написание кнопок к ним

--- ChooseModulePage.xaml.cs
+++ ChooseModulePage.xaml.cs
@@ -23,9 +23,77 @@ namespace IELTSAppProject
         public ChooseModulePage()
         {
             InitializeComponent();
+
+            foreach (UIElement elem in chosingModule.Children)
+            {
+                if (elem is Button)
+                {
+                    if (elem == turnBack)
+                        ((Button)elem).Click += turnBack_Click;
+                    if (elem ==statistic)
+                        ((Button)elem).Click += statistic_Click;
+                    if (elem == mistakes)
+                        ((Button)elem).Click += mistakes_Click;
+                    if (elem == collections)
+                        ((Button)elem).Click += collections_Click;
+                    if (elem == language)
+                        ((Button)elem).Click += language_Click;
+                    if (elem == varOfExam)
+                        ((Button)elem).Click += varOfExam_Click;
+                    if (elem == newCollections)
+                        ((Button)elem).Click += newCollections_Click;
+                    if (elem == testVar)
+                        ((Button)elem).Click += testVar_Click;
+                    if (elem == moduleTasks)
+                        ((Button)elem).Click += moduleTasks_Click;
+
+
+                }
+            }
+
         }
 
         private void turnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new FirstTestPage());
+        }
+
+        private void statistic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mistakes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void collections_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void language_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void varOfExam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void newCollections_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void testVar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void moduleTasks_Click(object sender, RoutedEventArgs e)
         {
 
         }
