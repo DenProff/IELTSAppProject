b1c7d088f46cb9699acfd89fceea239f6ede03a1
DenProff <farik29261@gmail.com>
2025-05-14 15:10:07 +0500

    Добавление страницы с выбором языка

---
+++ LanguageSelectionPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IELTSAppProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class LanguageSelectionPage : Page
+    {
+        public LanguageSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void russianLanguage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chineseLanguage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void englishLanguage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
