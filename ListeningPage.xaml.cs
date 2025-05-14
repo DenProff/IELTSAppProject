f8e6e1a1aa2b427f162c889ce12faf0dc3fee111
DenProff <farik29261@gmail.com>
2025-05-14 12:40:38 +0500

    Добавление функционала кнопки "Помощь" в разделе Listening

--- ListeningPage.xaml.cs
+++ ListeningPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace IELTSAppProject
 {
@@ -23,6 +26,14 @@ namespace IELTSAppProject
         public ListeningPage()
         {
             InitializeComponent();
+            this.KeyDown += (sender, e) =>
+            {
+                if (e.Key == Key.F1)
+                {
+                    OpenChmHelp();
+                    e.Handled = true;
+                }
+            };
         }
 
         private void SimpleAudioPlayer_Loaded(object sender, RoutedEventArgs e)
@@ -30,9 +41,32 @@ namespace IELTSAppProject
 
         }
 
-        public void help_Click(object sender, RoutedEventArgs e)
+        private void OpenChmHelp()
         {
+            string chmPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Help",
+                "referenceData.chm"
+            );
 
+            if (File.Exists(chmPath))
+            {
+                try
+                {
+                    // Открыть страницу "settings.html" внутри CHM
+                    Process.Start("hh.exe", $"{chmPath}::/listening.htm");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                                  MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void help_Click(object sender, RoutedEventArgs e)
+        {
+            OpenChmHelp();
         }
     }
 }
