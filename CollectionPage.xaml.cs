ad0f165b85e3738c8c4ecc4b3b0e65b4137a8ac1
Baiburov-Mikhail <mbaiburov@mail.ru>
2025-05-31 18:38:36 +0500

    Добавлена xaml для подборки. Содержит блок ItemsControl - туда будут подгружаться UserControl-ы

--- CollectionPage.xaml.cs
+++ CollectionPage.xaml.cs
@@ -28,20 +28,16 @@ namespace IELTSAppProject
         public CollectionPage()
         {
             InitializeComponent();
-            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            string projectRoot = Path.GetFullPath(Path.Combine(baseDir, @"..\.."));
-            string audio = Path.Combine(projectRoot, "audio", "cocoJambo.mp3");
-            byte[] data = SoundControl.ConvertMp3ToWavBytes(audio);
-            SpeakingTask task = new SpeakingTask("Is it worth buying an apartment on the first floor?", data, 5);
-            // Загружаем три разных типа заданий
-            SpeakingFrame.Navigate(new SpeakingPage(task));
-            ListeningFrame.Navigate(new ListeningPage());
-            WritingFrame.Navigate(new WritingPage());
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Check_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService?.Navigate(new FirstTestPage());
+
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
