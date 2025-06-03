0890d38efcc279dca1884f124c9d5117a6d23d08
Baiburov-Mikhail <mbaiburov@mail.ru>
2025-06-03 22:05:08 +0500

    Для класса JsonControl сделано статическое свойство, которое десериализует массив с заданиями и возвращает массив. В класс CollectionPage добавлен метод SearchForIndexById - Бинарный поиск по id; возвращается индекс элемента с искомым id в массиве.

--- CollectionPage.xaml.cs
+++ CollectionPage.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Path = System.IO.Path;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
 namespace IELTSAppProject
 {
@@ -28,20 +28,48 @@ namespace IELTSAppProject
         public CollectionPage(TaskCollection taskCollection)
         {
             InitializeComponent();
-            foreach (int task in taskCollection)
+
+            Task[] taskList = JsonControl.TaskList; // Десериализация в список json-файла со всеми заданиями
+
+            foreach (int taskId in taskCollection) // Перебор id, хранящихся в поле-списке TaskCollection
             {
-                // Обработка id-шек и подтягивание из json - не реализовано
+                // Подтягивание заданий из json по id при помощи бинарного поиска - не реализовано
+                int index = SearchForIndexById(ref taskList, taskId); // Поиск индекса задания с нужным id
+
             }
         }
 
-        private void Check_Click(object sender, RoutedEventArgs e)
+        private void Check_Click(object sender, RoutedEventArgs e) // Обработчик события кнопки "Проверить всё"
         {
 
         }
 
-        private void Convert_Click(object sender, RoutedEventArgs e)
+        private void Convert_Click(object sender, RoutedEventArgs e) // Обработчик события кнопки "Конвертировать всё в docx"
         {
 
+        }
+
+        private int SearchForIndexById(ref Task[] list, int id) // Бинарный поиск по id; возвращается индекс элемента с искомым id в массиве
+        {
+            int left = 0;
+            int right = list.Length;
+            int mid;
+            while (left < right)
+            {
+                mid = left + (right - left) / 2;
+                if (list[mid].Id >= id)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            if (left == id)
+                return left;
+
+            else throw new Exception("Задания с нужным id нет. Возможно была нарушена упорядоченность id по возрастанию в файле json.");
         }
     }
 }
---
+++ JsonControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace IELTSAppProject
+{
+    public abstract class JsonControl
+    {
+        public static Task[] TaskList // Свойство, возвращающее массив с заданиями
+        {
+            get
+            {
+                string projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                string file = Path.Combine(projectDir, "resourcesTask", "tasks", "tasks.json");
+                if (!File.Exists(file))
+                    throw new Exception("Файл с json не найден!");
+
+                // Чтение JSON из файла
+                string jsonFromFile = File.ReadAllText(file);
+
+                // Десериализация
+                return JsonConvert.DeserializeObject<Task[]>(jsonFromFile);
+            }
+            private set { }
+        }
+
+        public void AddTask() 
+        {
+            // Функция для добавления новых заданий
+        }
+    }
+}
